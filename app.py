7ebdb9f6ef345c8df8b26b1485cbd435d9d81236
Pedro Marques <123904334+PedroCarneiroMarques@users.noreply.github.com>
2026-05-29 21:52:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import pandas as pd
 import plotly.io as pio
 import streamlit as st
 
+import streamlit as st
+
+st.title("🚧 Maintenance mode")
+st.stop()
+
 from charts import build_festival_chart
 from components import (
     MANJERICO_HTML,
